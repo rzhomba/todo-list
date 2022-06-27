3ee170fec3664ef3e500d8378496edbe1b8d8dde
rzhomba <theviper0235@gmail.com>
2022-06-27 17:58:37 +0300

    Basic UI structure

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import './App.css'
+import Header from './Header'
+import TaskList from './Task/TaskList'
 
 const App = () => {
   return (
     <div>
+      <Header/>
+      <TaskList/>
     </div>
   )
 }
