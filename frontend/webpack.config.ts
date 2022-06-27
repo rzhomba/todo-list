b5839d6719e5abca715e6a80b974976b75b472c3
rzhomba <theviper0235@gmail.com>
2022-06-27 16:20:50 +0300

    Init

---
+++ frontend/webpack.config.ts
@@ -0,0 +1,37 @@
+import path from 'path'
+
+export default {
+  entry: './src/index.tsx',
+  devtool: 'inline-source-map',
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        loader: 'ts-loader'
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  resolve: { extensions: ['.tsx', '.ts', '.js'] },
+  output: {
+    path: path.resolve(__dirname, 'dist/'),
+    publicPath: '/dist/',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    historyApiFallback: true,
+    static: {
+      directory: path.join(__dirname, 'public/')
+    },
+    port: 8080,
+    devMiddleware: {
+      publicPath: `https://localhost:8080/dist/`
+    },
+    hot: true
+  }
+}
