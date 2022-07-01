f06316ca65a4333ed0ecb34a48d5504bfc345cff
rzhomba <theviper0235@gmail.com>
2022-07-01 21:14:54 +0300

    Axios init

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,6 +3,12 @@ import { createRoot } from 'react-dom/client'
 import App from './App'
 import { Provider } from 'react-redux'
 import { store } from './store'
+import axios from 'axios'
+import config from '../config.json'
+
+axios.defaults.baseURL = config.apiUrl
+axios.defaults.headers.post['Content-Type'] = 'application/json'
+axios.defaults.withCredentials = true
 
 const container = document.getElementById('root')
 const root = createRoot(container!)
