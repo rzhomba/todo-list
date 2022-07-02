c1dcea9adc41ad3dd31a850ac895883ef5512cc9
rzhomba <theviper0235@gmail.com>
2022-07-02 05:27:53 +0300

    Sync auth form render with state

--- frontend/src/Auth/AuthForm.tsx
+++ frontend/src/Auth/AuthForm.tsx
@@ -1,13 +1,24 @@
-import React, { useLayoutEffect } from 'react'
+import React, { useCallback, useLayoutEffect } from 'react'
 import { useAppSelector, useAppDispatch } from '../hooks'
 import { selectAuth, setLogin, setPassword, signIn } from './authSlice'
 import { useNavigate } from 'react-router-dom'
 import './AuthForm.css'
 
 const AuthForm = () => {
-  const { loggedIn } = useAppSelector(selectAuth)
+  const { loggedIn, authForm } = useAppSelector(selectAuth)
   const dispatch = useAppDispatch()
   const navigate = useNavigate()
+
+  const loginInputRef = useCallback((node: HTMLInputElement) => {
+    if (node !== null) {
+      node.value = authForm.login
+    }
+  }, [authForm.login])
+  const passwordInputRef = useCallback((node: HTMLInputElement) => {
+    if (node !== null) {
+      node.value = authForm.password
+    }
+  }, [authForm.password])
 
   const handleLoginChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     dispatch(setLogin(e.target.value))
@@ -28,8 +39,8 @@ const AuthForm = () => {
 
   return (
     <form className="auth-form" onSubmit={handleSubmit}>
-      <input type="text" placeholder="Login" onChange={handleLoginChange}/>
-      <input type="password" placeholder="password" onChange={handlePasswordChange}/>
+      <input type="text" placeholder="Login" name="test" onChange={handleLoginChange} ref={loginInputRef}/>
+      <input type="password" placeholder="password" onChange={handlePasswordChange} ref={passwordInputRef}/>
       <button type="submit" className="auth-submit">Sign In</button>
     </form>
   )
