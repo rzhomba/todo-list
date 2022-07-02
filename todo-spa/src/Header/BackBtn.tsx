819362f58d1e9afd375e9118696bfb88db4c4307
rzhomba <theviper0235@gmail.com>
2022-07-02 07:29:57 +0300

    Fix

--- todo-spa/src/Header/BackBtn.tsx
+++ todo-spa/src/Header/BackBtn.tsx
@@ -1,20 +1,15 @@
 import React from 'react'
-import { Link, useNavigate } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import './SignBtn.css'
 import { useAppSelector } from '../hooks'
 import { selectAuth } from '../Auth/authSlice'
 
 const BackBtn = () => {
   const { loggedIn } = useAppSelector(selectAuth)
-  const navigate = useNavigate()
-
-  const handleClick = () => {
-    navigate('/')
-  }
 
   return (
     <Link to="/">
-      <button className="sign-cancel" onClick={handleClick}>
+      <button className="sign-cancel">
         {loggedIn ? 'Return' : 'Cancel'}
       </button>
     </Link>
